ac5412052ec9ec2175f624f5fb26f363f616d337
糕糕 <56777256+dtgz-sudo@users.noreply.github.com>
2023-04-20 09:40:26 +0800

    Update train_base.py
    修改tensorflow 使用缓存

--- src/train_base.py
+++ src/train_base.py
@@ -2,7 +2,10 @@ import json
 import argparse
 from utils import *
 from DeepPHiC import DeepPHiC
-
+import tensorflow as tf 
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+for physical_device in physical_devices:
+    tf.config.experimental.set_memory_growth(physical_device, True)
 def train(tissues, args):
 
     for tissue in tissues:
